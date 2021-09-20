3065a47ddf26aa30edd8c3e9a7077555d76e4b17
Oleksandr Dolotii <dwin13672@gmail.com>
2021-09-19 21:54:26 -0400

    lastUpdate2021_09_19

--- certificates.js
+++ certificates.js
@@ -3,12 +3,20 @@ $(function () {
 
 $("#buttonQA").click(function(){
 $("#QAimage").slideToggle(1000)
-$("#QAimage").css({"width" : "400px", "border-width": "1px", "border-style":"solid", "border-color": "#1d469e", "border-radius": "2px"});
+$("#QAimage").css({"width" : "300px", "border-width": "1px", "border-style":"solid", "border-color": "#1d469e", "border-radius": "2px"});
 });
 
-$("#QAimage").click(function(){
-    $("#QAimage").hide(1000)
+$("#QAimage").hover(function(){
+
+    $("#QAimage").animate({
+        width: "350px",
+    },1000);
+
     });
+
+/*$("#QAimage").click(function(){
+    $("#QAimage").hide(1000)
+    });*/
 
 });
 
@@ -17,13 +25,20 @@ $(function () {
     let sqlImg =  $("#SQLimage")
     $("#buttonSQL").click(function(){
         sqlImg.slideToggle(1000)
-        sqlImg.css({"width" : "400px", "border-width": "1px", "border-style":"solid", "border-color": "#1d469e", "border-radius": "2px"});
+        sqlImg.css({"width" : "300px", "border-width": "1px", "border-style":"solid", "border-color": "#1d469e", "border-radius": "2px"});
     });
     
+    $("#SQLimage").hover(function(){
 
-    $("#SQLimage").click(function(){
-        sqlImg.hide(1000)
+        $("#SQLimage").animate({
+            width: "350px",
+        },1000);
+    
         });
+
+    /*$("#SQLimage").click(function(){
+        sqlImg.hide(1000)
+        });*/
     
     });
 
